35bfd8fced8ab0c4335d8aefdce07f81ce0629ab
Michael Brown and Mary McMurray <michael@>
2018-10-15 11:57:49 -0700

    add liter to gallon conversion

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,12 @@ var convertGaltoLtr = function (gallon) {
 var gallon = parseInt(prompt("How much does the recipe ask for in gallons?"));
 
 convertGaltoLtr(gallon);
+
+var convertLtrtoGal = function (liter) {
+	var gallon = liter/3.78541;
+  alert("The amount in gallons is: " + gallon);
+}
+
+var liter = parseInt(prompt("How much does the recipe ask for in liters?"));
+
+convertLtrtoGaltoLtr(liter);
