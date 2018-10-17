435cd6b4b5de10b0bb443ae858fc2ca1d6b9f2f3
Kristin Brewer-Lowe and Mary McMurray <kristin@>
2018-10-17 11:59:56 -0700

    add convert liters to gallons

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-var convertGaltoLtr = function (gallon) {
-	var liter = gallon*3.78541;
-  alert("The amount in liters is: " + liter);
-}
 
-var gallon = parseInt(prompt("How much does the recipe ask for in gallons?"));
+	var convertGaltoLtr = function(gallon) {
+		return gallon*3.78541;
+	}
 
-convertGaltoLtr(gallon);
+	var convertLtrtoGal = function (liter) {
+		return liter/3.78541;
+	}
+// 	convertGaltoLtr(gallon);
+//
 
-var convertLtrtoGal = function (liter) {
-	var gallon = liter/3.78541;
-  alert("The amount in gallons is: " + gallon);
-}
+// 	  alert("The amount in gallons is: " + gallon);
+// 	}
+//
+// 	var liter = parseInt(prompt("How much does the recipe ask for in liters?"));
+//
+// 	convertLtrtoGaltoLtr(liter);
+//
+// });
 
-var liter = parseInt(prompt("How much does the recipe ask for in liters?"));
-
-convertLtrtoGaltoLtr(liter);
+$(document).ready(function() {
+	$("#gal-to-liter").submit(function(event) {
+		event.preventDefault();
+		var gallon = parseInt($("input#gallon").val());
+		var result = convertGaltoLtr(gallon);
+		$(".output").text(result);
+	});
+	$("#liter-to-gallon").submit(function(event) {
+		event.preventDefault();
+		var liter = parseInt($("input#liter").val());
+		var result = convertLtrtoGal(liter);
+		$(".output2").text(result);
+	});
+});
